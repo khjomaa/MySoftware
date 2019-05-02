52834b1c95a8d557742dfb646d46f8610b2e4a5d
khjomaa <khjomaa@gmail.com>
2019-05-02 19:51:27 +0300

    added welcome function (#2)

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ def main():
     print("Hello World!")
 
 
+def welcome():
+    print("New Screen")
+
+    
 def click():
     print("Click")
 
 
 if __name__ == "__main__":
     main()
+    welcome()
     click()
